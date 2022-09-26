173baa5ff35f71c36f26be12b2a6fa220e4b06f5
key <key9559@naver.com>
2022-09-26 17:56:37 +0900

    scroll

--- js/ui.js
+++ js/ui.js
@@ -7,6 +7,7 @@ gnbWrap.append(`
 	<a href="buttons.html" class="gnb-link">버튼모음</a>
 	<a href="inputs.html" class="gnb-link">인풋모음</a>
 	<a href="animations.html" class="gnb-link">애니메이션모음</a>
+	<a href="scrolls.html" class="gnb-link">스크롤모음</a>
 </div>`);
 
 const gnbLink = $(".gnb-link");
@@ -17,4 +18,6 @@ if ($("#page").hasClass("button-page")) {
   gnbLink.eq(2).addClass("on");
 } else if ($("#page").hasClass("ani-page")) {
   gnbLink.eq(3).addClass("on");
+} else if ($("#page").hasClass("scroll-page")) {
+  gnbLink.eq(4).addClass("on");
 }
